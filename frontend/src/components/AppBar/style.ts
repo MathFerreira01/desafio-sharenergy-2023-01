d304eb4bd8637ff3df7dd654e5de7543fd13b8a1
MathFerreira01 <ferreiramatheus124@gmail.com>
2023-01-05 19:20:21 -0300

    feat: criado a estilização do component AppBar

--- frontend/src/components/AppBar/style.ts
+++ frontend/src/components/AppBar/style.ts
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    
+    width: 100%;
+    padding: 0.8em;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: black;
 `;
